658db059219d365a06b24703bc64585110ceec96
Leyna Girardi <leyna.girardi@heig-vd.ch>
2020-11-06 14:29:48 +0100

    APIDoc created auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,42 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const config = require('../config');
 
-/* Authentication */
+/**
+ * @api {post} /login Login of users
+ * @apiName Login
+ * @apiGroup LoginUser
+ * @apiVersion 1.0.0
+ * @apiDescription Login of users
+ *
+ * @apiParam (Request body) {String{/^[a-zA-Z0-9]+$/}} username The username of the user 
+ * @apiParam (Request body) {String} The password of the user  (that is hashed in db)
+ * 
+ * @apiSuccess (Response body) {String} token The token of the user of the session (last 1 week)
+ * 
+ * @apiError {Object} 401/Unauthorized 
+ * @apiErrorExample {json} 401 Unauthorized
+ *     HTTP/1.1 401 Unauthorized
+ *     Content-Type: text/plain
+ * 
+ *     Unauthorized
+ *
+ * @apiExample Example
+ *     POST /login HTTP/1.1
+ *     Content-Type: application/json
+ *
+ *    {
+ *       "username": "Pomme",
+ *       "password": "Tre$B0n"
+ *    }
+ *
+ * @apiSuccessExample 200 OK
+ *     HTTP/1.1 200 OK
+ *     Content-Type: application/json
+ *
+ *    {
+ *      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1Zjk4MWU2NGVlYWMzMDQyYjBlMjdiODYiLCJleHAiOjE2MDUyNzM5ODUuNDk5LCJpYXQiOjE2MDQ2NjkxODV9.HpC2MibTEzGBh2s21Otc6eAQZ2KGwbkqvO3XssHFhNA"
+ *    }
+ */
 router.post('/', function(req, res, next) {
 
   User.findOne({
