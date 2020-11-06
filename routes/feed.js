2de43b2e3d5780b9044e37732d06b0d00c2ad9d9
Leyna Girardi <leyna.girardi@heig-vd.ch>
2020-11-06 15:22:43 +0100

    APIDoc created feed.js

--- routes/feed.js
+++ routes/feed.js
@@ -24,6 +24,65 @@ const List = require('../models/list');
  * Example : http://localhost:4000/feed?page=1&pageSize=1
  */
 
+ /**
+ * @api {get} /feed Feed of all pictures
+ * @apiName RetrieveFeed
+ * @apiGroup Feed
+ * @apiVersion 1.0.0
+ * @apiDescription Retrieves a paginated list of all pictures ordered by title (in alphabetical order).
+ *
+ * @apiSuccess (Response body) {String{3...50}} description The description of the picture
+ * @apiSuccess (Response body) {String} location The location of the picture
+ * @apiSuccess (Response body) {Number} coordinates Coordinates of the picture
+ * @apiSuccess (Response body) {String} picture The url of the picture
+ * @apiSuccess (Response body) {Date} creation_date The date of the picture's creation
+ * @apiSuccess (Response body) {Date} last_mod_date The date at which the picture was modified
+ * @apiSuccess (Response body) {Schema.Types.ObjectId} userId An Id which is referencing to the user who create the picture
+ *
+ * @apiParam (URL query parameters) {Date} [min_date] Select only pictures before this specified date 
+ * @apiParam (URL query parameters) {Date} [max_date] Select only pictures after this specified date 
+ *
+ * @apiExample Example
+ *     GET /feed?page=1&pageSize=1 HTTP/1.1
+ *
+ * @apiSuccessExample 200 OK
+ *     HTTP/1.1 200 OK
+ *     Content-Type: application/json
+ *     Link: https://odos-archioweb.herokuapp.com/feed?page=1&pageSize=1;"
+ *
+ *    [
+ *       {
+ *         "location": {
+ *           "type": "Point",
+ *           "coordinates": [
+ *               48.862725,
+ *               2.287592
+ *           ]
+ *       },
+ *         "description": "1 picture",
+ *         "picture": "https://source.unsplash.com/random",
+ *         "creation_date": "2020-11-04T14:36:46.995Z",
+ *         "last_mod_date": "2020-11-04T14:36:46.995Z",
+ *         "userId": "5fa198479bb5b63b249b06af",
+ *         "id": "5fa2bc7ec26b8d62e8e1b205"
+ *      },
+ *      {
+ *         "location": {
+ *           "type": "Point",
+ *           "coordinates": [
+ *               48.862725,
+ *               2.287592
+ *           ]
+ *      },
+ *         "description": "2 picture",
+ *         "picture": "https://source.unsplash.com/random",
+ *         "creation_date": "2020-11-03T18:33:09.438Z",
+ *         "last_mod_date": "2020-11-03T18:33:09.438Z",
+ *         "userId": "5fa198479bb5b63b249b06af",
+ *         "id": "5fa1a265b941dc1f4096ced3"
+ *      }
+ *    ]
+ */
 router.get('/', function(req, res, next) {
   // Count total pictures matching the URL query parameters
   const countQuery = queryPictures(req);
@@ -89,5 +148,6 @@ function queryPictures(req) {
 
   return query;
 }
+
 
 module.exports = router;
