efb95d0372edca792d3739c9a78f552e8227c98f
Soraya <soraya.meystre@heig-vd.ch>
2020-11-18 15:12:08 +0100

    Edit WebSocket

--- websocket/dispatcher.js
+++ websocket/dispatcher.js
@@ -2,7 +2,9 @@
 const WebSocket = require('ws');
 
 // ------ LOGGER ------
-const { createLogger } = require('../config');
+const {
+  createLogger
+} = require('../config');
 
 // ------ MODELS ------
 const Picture = require('../models/picture');
@@ -14,56 +16,62 @@ let tabCreateUsers = [];
 
 // ------ FUNCTIONS ------
 exports.createBackendDispatcher = function(server) {
-    // SETUP
-    const logger = createLogger('Websocket Odos');
+  // SETUP
+  const logger = createLogger('Websocket Odos');
 
-    // COMMUNICATIONS
-    const wss = new WebSocket.Server({
-        server
+  // COMMUNICATIONS
+  const wss = new WebSocket.Server({
+    server
+  });
+
+  // Handle new user connections.
+  wss.on('connection', function(ws) {
+    logger.info('New WebSocket user connected');
+    tabCreateUsers.push(ws);
+
+    // Forget the mapping when the client disconnects.
+    ws.on('close', () => {
+      logger.info(`You are disconnected`);
+      tabCreateUsers.splice(tabCreateUsers.indexOf(ws), 1);
     });
-
-    // Handle new user connections.
-    wss.on('connection', function(ws) {
-        logger.info('New WebSocket user connected');
-        tabCreateUsers.push(ws);
-
-        // Forget the mapping when the client disconnects.
-        ws.on('close', () => {
-            logger.info(`You are disconnected`);
-            tabCreateUsers.splice(tabCreateUsers.indexOf(ws), 1);
-        });
-    });
+  });
 };
 
 // Counting the number of users
-exports.nbUsers = function(users){
-    tabCreateUsers.forEach(ws => {
-        ws.send('There are ' + users + ' users');
-    })
+exports.nbUsers = function(users) {
+  tabCreateUsers.forEach(ws => {
+    ws.send('There are ' + users + ' users');
+  })
 };
 
 // Counting the number of pictures
-exports.nbPictures = function(){
+exports.nbPictures = function() {
   Picture.count(function(err, count) {
     if (err) {
       return;
     }
     tabCreateUsers.forEach(ws => {
-      ws.send('There are ' + count + ' pictures');
+      ws.send(JSON.stringify({
+        message: 'pictureCountUpdated',
+        data: count
+      }));
     })
   });
 };
 
-// Get the new pictureNotFound
-exports.newPicture = function(picture){
+// Get the new picture
+exports.newPicture = function(picture) {
   tabCreateUsers.forEach(ws => {
-      ws.send('There is a new picture: ' + picture);
+    ws.send(JSON.stringify({
+      message: 'pictureCreated',
+      data: picture
+    }));
   })
 };
 
 // Counting the number of lists
-exports.nbLists = function(lists){
-    tabCreateUsers.forEach(ws => {
-        ws.send('There are ' + lists + ' lists');
-    })
+exports.nbLists = function(lists) {
+  tabCreateUsers.forEach(ws => {
+    ws.send('There are ' + lists + ' lists');
+  })
 };
